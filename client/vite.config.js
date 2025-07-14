10e4030636c7da2d7d8365c5570750fafc5f70d1
Sanjiv Joshi <sanjivjoshi97@gmail.com>
2025-07-13 20:18:59 -0400

    client: remove icon and update metadata, add comment about proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    /* Define a backend proxy */
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
